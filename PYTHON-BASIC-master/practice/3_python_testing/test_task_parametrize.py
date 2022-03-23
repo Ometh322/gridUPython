74962ad371b5fd7053a16b09493b2b01c6868a5d
Ometh322 <174340Do1sar1>
2022-03-23 22:47:40 +0400

    completed test_task_parametrize.py

--- PYTHON-BASIC-master/practice/3_python_testing/test_task_parametrize.py
+++ PYTHON-BASIC-master/practice/3_python_testing/test_task_parametrize.py
@@ -9,17 +9,34 @@ Task:
  1. Write a test with @pytest.mark.parametrize decorator.
  2. Find the buggy function and fix it.
 """
+import pytest
 
 
 def fibonacci_1(n):
     a, b = 0, 1
-    for _ in range(n-1):
+
+    # Fixed part:
+    if n in {0, 1}:
+        return n
+
+    for _ in range(2, n + 1):
         a, b = b, a + b
     return b
 
 
 def fibonacci_2(n):
     fibo = [0, 1]
-    for i in range(1, n+1):
-        fibo.append(fibo[i-1] + fibo[i-2])
+    # Fixed starting number of range (1->2)
+    for i in range(2, n + 1):
+        fibo.append(fibo[i - 1] + fibo[i - 2])
     return fibo[n]
+
+
+@pytest.mark.parametrize("test_input,expected", [(1, 1), (7, 13), (8, 21), (0, 0)])
+def test_fibonacci_1(test_input, expected):
+    assert fibonacci_1(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [(1, 1), (7, 13), (8, 21), (0, 0)])
+def test_fibonacci_2(test_input, expected):
+    assert fibonacci_2(test_input) == expected
