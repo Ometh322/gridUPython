f024371f3ab84761f76decaecf45de78986d2483
Ometh322 <174340Do1sar1>
2022-03-24 13:21:28 +0400

    moved objects initialization to the setUp method

--- PYTHON-BASIC-master/practice/3_python_testing/test_task_classes.py
+++ PYTHON-BASIC-master/practice/3_python_testing/test_task_classes.py
@@ -12,28 +12,26 @@ import datetime
 
 
 class TestClasses(unittest.TestCase):
+    def setUp(self) -> None:
+        self.teacher = Teacher('Dmitry', 'Orlyakov')
+        self.student = Student('Vladislav', 'Popov')
+
     def test_correct_names(self):
-        teacher = Teacher('Dmitry', 'Orlyakov')
-        student = Student('Vladislav', 'Popov')
-        self.assertEqual('Dmitry', teacher.last_name)
-        self.assertEqual('Popov', student.first_name)
+        self.assertEqual('Dmitry', self.teacher.last_name)
+        self.assertEqual('Popov', self.student.first_name)
 
     def test_correct_homework(self):
-        teacher = Teacher('Dmitry', 'Orlyakov')
-        student = Student('Vladislav', 'Popov')
         deadline: datetime.timedelta = datetime.timedelta(days=0)
         text: str = 'Learn functions'
-        expired_homework = teacher.create_homework('Learn functions', 0)
+        expired_homework = self.teacher.create_homework('Learn functions', 0)
         self.assertEqual(deadline, expired_homework.deadline)
         self.assertEqual(text, expired_homework.text)
         self.assertEqual(False, expired_homework.is_active())
-        self.assertEqual(None, student.do_homework(expired_homework))
+        self.assertEqual(None, self.student.do_homework(expired_homework))
 
     def test_negative_days(self):
-        teacher = Teacher('Dmitry', 'Orlyakov')
-        student = Student('Vladislav', 'Popov')
         deadline: datetime.timedelta = datetime.timedelta(days=0)
-        negative_days_homework = teacher.create_homework('Negative', -5)
+        negative_days_homework = self.teacher.create_homework('Negative', -5)
         negative_deadline: datetime.timedelta = datetime.timedelta(days=-5)
         self.assertEqual(negative_deadline, negative_days_homework.deadline)
 
